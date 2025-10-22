25cd8aadfe9956bde0a61fa6cd7cf06964421e79
Alfandrag <maxence.du.parc@gmail.com>
2025-10-22 11:53:54 +0200

    v1 de la page de création de compte

--- bdd/script_bdd.sql
+++ bdd/script_bdd.sql
@@ -25,7 +25,7 @@ CREATE TABLE Users(
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    pssword VARCHAR(255) NOT NULL,
     cat INT,
     FOREIGN KEY (cat) REFERENCES Categories(id)
 )
