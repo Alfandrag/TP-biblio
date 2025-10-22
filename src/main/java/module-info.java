932c71c4a66efccf6ed48900048ac3eee03cfaa1
alfandrag <maxence.du.parc@gmail.com>
2025-10-22 08:44:35 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.tpbiblio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.tpbiblio to javafx.fxml;
+    exports org.example.tpbiblio;
+}
