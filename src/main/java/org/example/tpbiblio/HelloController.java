94d1bdefa5f17ede568bfff422c9e26719d4f4ae
Alfandrag <maxence.du.parc@gmail.com>
2025-11-02 15:51:36 +0100

    ajout de la page de profile et ouverture depuis la gae de base

--- src/main/java/org/example/tpbiblio/HelloController.java
+++ src/main/java/org/example/tpbiblio/HelloController.java
@@ -1,7 +1,11 @@
 package org.example.tpbiblio;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -10,5 +14,19 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void profile_press() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("profile.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+            stage.setTitle("Profil");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
